22545aaa85e0f7913a24fbeb7260ab341e5f7008
marcopoddi03 <marcopoddi03@gmail.com>
2022-11-10 18:14:06 +0100

    Connessione e invio msg client

---
+++ ImpiccatoClient/ImpiccatoClient/SocketClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ClientImpiccato
+{
+    public class SocketClient
+    {
+        Socket sender;
+        bool connected;
+        string connectedTo;
+
+        public SocketClient()
+        {
+            connected = false;
+        }
+        public void Connect()
+        {
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+            sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                sender.Connect(remoteEP);
+                connected = true;
+                connectedTo = sender.RemoteEndPoint.ToString();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void SendMsg(string s)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(s);
+            int bytesSent = sender.Send(msg);
+        }
+
+    }
+}
