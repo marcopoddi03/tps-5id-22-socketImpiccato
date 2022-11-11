b42b50b8e2d3d595770d2d76e63897f59a031cf5
marcopoddi03 <marcopoddi03@gmail.com>
2022-11-11 13:40:38 +0100

    classe parola

---
+++ ImpiccatoClient/ImpiccatoClient/Parola.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpiccatoClient
+{
+    public class Parola
+    {
+        int length;
+        char[] parola;
+        public Parola(int length)
+        {
+            this.length = length;
+            for (int i = 0; i < length; i++)
+                parola[i]= '-';
+        }
+
+        public void AggiornaParola(string s)
+        {
+            char[] m = s.ToCharArray();
+            for(int i=0;i<m.Length;i++)
+            {
+                if(i!=0)
+                {
+                    parola[m[i]] = m[0];
+                }
+            }
+        }
+    }
+    
+}
